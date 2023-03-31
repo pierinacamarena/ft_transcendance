c45d2b78de6362bb8b101015c0a6b8870d5637d5
pierinacamarena <pcamaren@student.42.fr>
2023-03-31 18:41:10 +0200

    modified dockerfile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World, shupi rocks
+          Hello World, Tunki time!
         </a>
       </header>
     </div>
